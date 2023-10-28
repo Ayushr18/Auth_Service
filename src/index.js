1ef07e3116d8e8afd057be606cf96ff4a09fdead
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2023-10-28 16:13:02 +0530

    Added jwt create and verify

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const {PORT} = require('./config/server-config');
 const apiRoutes =  require('./routes/index');
 
+//const UserService = require('./services/user-service');
 //const UserRepository = require('./repository/user-repository');
 
 const app = express();
@@ -20,8 +21,12 @@ const prepareAndStartServer = () => {
     app.listen(PORT, async () => {
         console.log(`Server Started on PORT: ${PORT}`);
 
-        // const repo = new UserRepository();
-        // const response = await repo.getById(1);
+         //const service = new UserService();
+        // const newToken = service.createToken({email: 'ayushsunilraj@gmail.com', id: 1})
+        // console.log("new token is", newToken);
+
+        // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImF5dXNoc3VuaWxyYWpAZ21haWwuY29tIiwiaWQiOjEsImlhdCI6MTY5ODQ4ODk0NSwiZXhwIjoxNjk4NDkyNTQ1fQ.POz_rNPsLDTK4gwUYmkaa1rCxG6yZlYtUjUleOYNlIA'
+        // const response = service.verifyToken(token);
         // console.log(response);
     });
 }
