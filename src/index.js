0080c2578dbd62a4411af8a32501b2c6057775fb
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2023-10-28 12:22:10 +0530

    Added user by id

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/server-config');
-
 const apiRoutes =  require('./routes/index');
+
+//const UserRepository = require('./repository/user-repository');
 
 const app = express();
 
@@ -16,8 +17,12 @@ const prepareAndStartServer = () => {
 
     app.use('/api', apiRoutes);
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server Started on PORT: ${PORT}`);
+
+        // const repo = new UserRepository();
+        // const response = await repo.getById(1);
+        // console.log(response);
     });
 }
 
