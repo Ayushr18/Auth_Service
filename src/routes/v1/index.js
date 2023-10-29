b10b358137c7112073dda78354a44c39ae3c62f1
Ayushr18 <121632238+Ayushr18@users.noreply.github.com>
2023-10-29 12:43:58 +0530

    Added user isAuth function

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -8,4 +8,8 @@ const router = express.Router();
 router.post('/signup',AuthRequestValidator.validateUserAuth, UserController.create);
 router.post('/signin',AuthRequestValidator.validateUserAuth, UserController.signIn);
 
+router.get('/isAuthenticated', UserController.isAuthenticated);
+
+
+
 module.exports = router;
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -37,6 +37,23 @@ class UserService {
         }
     }
 
+    async isAuthenticated(token) {
+        try {
+            const response = this.verifyToken(token);
+            if(!response) {
+                throw {error: 'Invalid token'}
+            }
+            const user = this.userRepository.getById(response.id);
+            if(!user) {
+                throw {error: 'No user the corresponding token exists'};
+            }
+            return user.id;
+        } catch (error) {
+            console.log("something went wrong in sign-in process");
+            throw error;
+        }
+    }
+
     createToken(user) {
         try {
            const result = jwt.sign(user,JWT_KEY, {expiresIn: '1h'});
